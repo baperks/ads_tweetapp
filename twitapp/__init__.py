af8d8428aeca0c89ca72ad7319d3fb5c0e616604
baperks <babc.perkins@gmail.com>
2021-11-17 21:43:47 -0500

    Create basic app function, directory structure, gitignore file

---
+++ twitapp/__init__.py
@@ -0,0 +1,32 @@
+# Flask app for Twitter
+import os
+from flask import Flask, app, render_template
+# from flask_bootstrap import Bootstrap
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'twitapp.sqlite'),
+    )
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+
+# bootstrap = Bootstrap(app)
+
+    @app.route('/hello')
+    def hello():
+        return render_template('index.html')
+
+
+    return app
