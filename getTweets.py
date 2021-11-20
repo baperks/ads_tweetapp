caf91117c54abf49d04751e6f0c544b87e5684f5
baperks <babc.perkins@gmail.com>
2021-11-19 22:33:50 -0500

    Add SQLite db file, set getTweets script to run on a scheduled basis; adjust base template

--- getTweets.py
+++ getTweets.py
@@ -1,9 +1,44 @@
+from time import timezone
+from twitconfig import *
+import tweepy
+from datetime import datetime, timezone, timedelta
+import preprocessor as p
+import pandas as pd
+import sqlite3 as sql
 
 
+auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 
+api  = tweepy.API(auth, wait_on_rate_limit=True)
+
+today = datetime.now(timezone.utc) - timedelta(minutes=15)
+yesterday = today - timedelta(days=1)
 
 client = tweepy.Client(bearer_token=TWITTER_BEARER_TOKEN, consumer_key=TWITTER_CONSUMER_KEY, 
                     consumer_secret=TWITTER_CONSUMER_SECRET, access_token=TWITTER_ACCESS_TOKEN,
                     access_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
 
-tweets = client.search_recent_tweets("(Apple iPhone) lang:en -retweet", end_time=today, max_results=100, start_time=yesterday, tweet_fields=['id', 'text', 'created_at', 'lang'])
+tweets = client.search_recent_tweets("(Taco Bell) lang:en -retweet", end_time=today, max_results=100, start_time=yesterday, tweet_fields=['id', 'text', 'created_at', 'lang'])
+
+def getTweets(twList):
+        output = []
+        for tweet in twList:
+                
+                text = tweet.text
+                created_at = tweet.created_at
+                lang = tweet.lang
+
+                line = {'text' : text, 'created_at' : created_at, 'lang' : lang}
+
+                output.append(line)
+        return output
+
+tweet_list = getTweets(tweets.data)
+df = pd.DataFrame(tweet_list)
+
+conn = sql.connect('data/tweets.db')
+df.to_sql('tweets', conn, if_exists='append')
+
+# df.to_csv( f'data/tweets{today}.csv')
+
