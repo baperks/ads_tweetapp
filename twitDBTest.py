2e4275451f2bc8dd783cd29727e562460a6b6457
baperks <babc.perkins@gmail.com>
2021-11-22 20:57:50 -0500

    Add test script to monitor tweet ingest

--- twitDBTest.py
+++ twitDBTest.py
@@ -5,7 +5,10 @@ conn = sql.connect('data/tweets.db')
 
 twits = pd.read_sql('SELECT COUNT(DISTINCT(ID)) AS num_tweets FROM tweets ORDER BY created_at DESC', conn)
 
-twits2 = pd.read_sql('SELECT ID, text FROM tweets ORDER BY created_at DESC LIMIT 20', conn)
+twits2 = pd.read_sql('SELECT * FROM tweets ORDER BY created_at DESC LIMIT 10', conn)
+
+latest = pd.read_sql('SELECT ID, MIN(created_at) AS min, MAX(created_at) AS max FROM tweets', conn)
 
 print(twits)
 print(twits2)
+print(latest)
